880a2257d6d8cd67ed2d5ed2067ff43850b6d5b5
Shubham <sraj94710@gmail.com>
2023-03-17 20:59:45 +0000

    Back to Hello world

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World from automated deployment.');
+  res.send('Hello World');
 });
 
 /* istanbul ignore next */
